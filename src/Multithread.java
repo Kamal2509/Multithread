61a1a4037da8394725d49bb6c8d0543667f604db
kamal2509 <you@example.com>
2023-04-14 23:22:29 +0530

    multithread

--- src/Multithread.java
+++ src/Multithread.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class Multithread {
+public class Multithread  extends Thread{
+    public void run()
+    {
+        System.out.println("i m first thread");
+    }
+}
+class  Test{
+    public static void main(String[] args) {
+        Multithread m=new Multithread();
+        m.start();
+    }
 }
